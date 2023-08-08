065f262c1ed17e1a15ae1aa5d22ae9ed219a09cb
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-08-09 06:37:39 +0900

    fix: Head -> head

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,11 +6,11 @@ import Carousel from "../components/Carousel";
 const Main = () => {
   return (
     <div className="h-fit">
-      <Head>
+      <head>
         <title>사육신 공식 홈페이지</title>
         <meta name="description" content="사육신 건강지킴이 연구소 사이트에 오신 것을 환영합니다!" />
         <meta content='/banner.jpeg' property="og:image" />
-      </Head>
+      </head>
 
       <Carousel images={['/banner.jpeg']} />
       <Link href='https://www.nhis.or.kr/static/alim/paper/oldpaper/202203/sub/01.html' className="bg-stone-800 h-60 w-60 mx-auto rounded-full font-bold text-white text-4xl flex items-center my-3 justify-center">
