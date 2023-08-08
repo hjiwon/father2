5fbcaa0af61532f89549ba03aa93a6691f1d9b75
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-08-09 05:17:03 +0900

    fix: fix spell error (NavBar to Navbar)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import NavBar from './components/NavBar'
+import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 export const metadata = {
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <body className="">
-      <NavBar />
+      <Navbar />
       {children}
       <Footer />
     </body>
