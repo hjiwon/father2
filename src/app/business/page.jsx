3a388d128926726098dcb561257ae71488f25fdb
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-08-10 12:02:27 +0900

    feat: 사업 안내 페이지 레이아웃 변경
    wave css 추가

--- src/app/business/page.jsx
+++ src/app/business/page.jsx
@@ -1,9 +1,10 @@
 const Business = () => {
   return (
     <div className="my-8 flex flex-col items-center">
-      <h1 className='text-4xl'>
-        사업안내
-      </h1>
+      <h1 className='text-4xl w-5/6'>
+        사업안내 <br /> <br />
+      </h1>      
+
 
       <div className="w-5/6 px-20 py-8 my-8 flex flex-col border border-stone-500 border-2 rounded-large border-dashed">
         <div className="text-2xl font-bold">
@@ -19,7 +20,7 @@ const Business = () => {
         <div className="w-full flex gap-4">
           <div className='w-1/3 flex flex-col items-center'>
           <p className='text-xs w-full border border-stone-300 border-2 border-dotted rounded-large py-5 text-center'>
-            <strong>건강관련 조사연구</strong>
+            <strong className="text-lg">건강관련 조사연구</strong>
             <br />
             <br />
             <span>각종 제도 연구 및 조사</span>
@@ -33,7 +34,7 @@ const Business = () => {
           <div className="h-10 w-0 border-l-2 border-dotted" />
 
           <p className='text-xs w-full border border-stone-300 border-2 border-dotted rounded-large py-5 text-center'>
-            <strong>사회복지제도 홍보</strong>
+            <strong className="text-lg">사회복지제도 홍보</strong>
             <br />
             <br />
             <span>나의 맞춤형 복지지원 찾아보기</span>
@@ -45,7 +46,7 @@ const Business = () => {
           <div className="h-10 w-0 border-l-2 border-dotted" />
 
           <p className='text-xs w-full border border-stone-300 border-2 border-dotted rounded-large py-5  text-center'>
-            <strong>교육ㆍ건강강좌</strong>
+            <strong className="text-lg">교육ㆍ건강강좌</strong>
             <br />
             <br />
             <span>건강관련 전문기관 등 연계</span>
@@ -61,7 +62,7 @@ const Business = () => {
 
           <div className='w-1/3 flex flex-col items-center'>
           <p className='text-xs w-full border border-stone-300 border-2 border-dotted rounded-large py-5  text-center'>
-            <strong>복지 사각지대 이웃 발굴 지원</strong>
+            <strong className="text-lg">복지 사각지대 이웃 발굴 지원</strong>
             <br />
             <br />
             <span>구제 가능한 제도 지원</span>
@@ -76,7 +77,7 @@ const Business = () => {
           <div className="h-10 w-0 border-l-2 border-dotted" />
 
           <p className='text-xs w-full border border-stone-300 border-2 border-dotted rounded-large py-5 text-center'>
-            <strong>사랑의 마음 운동</strong>
+            <strong className="text-lg">사랑의 마음 운동</strong>
             <br />
             <br />
             <span>지역사회자원과 연계한 이웃사촌 만들기</span>
@@ -90,7 +91,7 @@ const Business = () => {
 
           <div className='w-1/3 flex flex-col items-center'>
           <p className='text-xs w-full border border-stone-300 border-2 border-dotted rounded-large py-5  text-center'>
-            <strong>자원봉사자와 함께하는 작은 음악회</strong>
+            <strong className="text-lg">자원봉사자와 함께하는 작은 음악회</strong>
             <br />
             <br />
             <span>정신적 건강을 위한 지원</span>
@@ -102,7 +103,7 @@ const Business = () => {
           <div className="h-10 w-0 border-l-2 border-dotted" />
 
           <p className='text-xs w-full border border-stone-300 border-2 border-dotted rounded-large py-5 text-center'>
-            <strong>환경지킴이 활동</strong>
+            <strong className="text-lg">환경지킴이 활동</strong>
             <br />
             <br />
             <span>환경보호 캠페인</span>
