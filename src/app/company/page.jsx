1179be42b2a7a45d0dc7c3009b25d2631aefbf3c
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-08-11 17:20:56 +0900

    feat: 인사말, 대표자 소개 병합

--- src/app/company/page.jsx
+++ src/app/company/page.jsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 const Hello = () => {
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center mb-20">
 
           <h1 className='text-4xl font-bold mt-20 mb-5'>대표자 인사말</h1>
           <div className="h-10 w-1 border border-x-2 border-stone-400 mb-10"/>
           
-          <div className="flex my-10 gap-20">
+          <div className="flex my-10 gap-20 pb-20 border-b-4">
             <div className="w-1/2 flex items-center justify-center">
               <Image className="w-80 my-10 object-contain" src="/Hello.jpeg" alt="" width={1000} height={1000} />
             </div>
@@ -36,6 +36,36 @@ const Hello = () => {
               여러분들의 적극적인 협력과 지지를 당부드립니다. <br /> <br />
 
               건강하세요! <br /> <br />
+            </div>
+          </div>
+
+
+        <h1 className='text-4xl font-bold mt-20 mb-5'>대표자 소개</h1>
+          <div className="h-10 w-1 border border-x-2 border-stone-400 mb-10"/>
+
+
+          <div className="flex items-center my-10 gap-20">   
+            <div className="w-1/2 flex items-center justify-center">     
+              <Image className="w-80 mt-16 object-contain" src="/Info2.jpeg" alt="" width={1000} height={1000} />
+            </div>
+
+            <div className="break-words w-1/2">
+              <span className="text-2xl font-semibold">황순창 </span>
+              <span className="text-base text-gray-500 font-semibold">黃淳昶, Sunchang Hwang <br /> <br /></span>
+            
+              <span >
+                (현)&nbsp;&nbsp;&nbsp;&nbsp;{`사육신 건강지킴이 연구소 대표`} <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`채용 · 승진 등 면접관`}<br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`화성특례시 준비위원회 의원`}<br /> <br />
+
+                (전) &nbsp;&nbsp;&nbsp;{`국민건강보험공단 지사장`} <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`국민건강보험공단 감사실`}<br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`용인시 지역보건의료심의회의원`}<br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`안양시 사회복지협의체 부회장`}<br /> <br />
+
+                (학력) 서강대학교 MBA 내부감사 전문과정 수료 <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`강남대학교 사회복지학 · 행정학 전공`}
+              </span>
             </div>
           </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,11 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div className="bg-stone-800 h-20 flex items-center p-5 w-screen text-2xl">
+    <div className="bg-stone-800 h-20 flex items-center p-5 w-screen text-3xl">
       <ol className="flex gap-6 items-center p-2 text-stone-200">
         <Link href='/' className="hover:underline hover:underline-offset-8">홈</Link>
         <Link href='/business' className="hover:underline hover:underline-offset-8">사업안내</Link>
         <Link href='/company' className="hover:underline hover:underline-offset-8">인사말</Link>
-        <Link href='/info' className="hover:underline hover:underline-offset-8">대표자 소개</Link>
         <Link href='/notion' className="hover:underline hover:underline-offset-8">대내외 활동</Link>
         <Link href='/photozone' className="hover:underline hover:underline-offset-8">포토존</Link>
         <Link href='/health' className="hover:underline hover:underline-offset-8">정보방</Link>
